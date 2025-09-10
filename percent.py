8bc59d635b1d11656e23aaaca26c1c164c39bbec
TokaiTomo <tokai.tomonari@gmail.com>
2025-09-10 19:23:27 +0700

    commit

---
+++ percent.py
@@ -0,0 +1,8 @@
+score_1 = float(input("Enter out of 50 how many was correct: "))
+score_2 = float(input("Enter out of 50 how many was correct: "))
+score_3 = float(input("Enter out of 50 how many was correct: "))
+score_4 = float(input("Enter out of 50 how many was correct: "))
+score_5 = float(input("Enter out of 50 how many was correct: "))
+sum = score_1+score_2+score_3+score_4+score_5
+percentage = sum/250*100
+print("The percentage of all of the scores is ", percentage, "%")
