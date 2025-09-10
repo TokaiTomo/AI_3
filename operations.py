198346ddd949f21ffa3fb5a6d4f0f2036f82a236
TokaiTomo <tokai.tomonari@gmail.com>
2025-09-10 19:12:48 +0700

    stuff

---
+++ operations.py
@@ -0,0 +1,11 @@
+num_1 = int(input("Please input your first number: "))
+num_2 = int(input("Please input your second number: "))
+num_3 = int(input("Please input your third number: "))
+num_4 = int(input("Please input your fourth number: "))
+num_5 = int(input("Please input your fifth number: "))
+sum = num_1+num_2+num_3+num_4+num_5
+
+print("The sum of the numbers is ", sum)
+
+average = (sum / 5)
+print("The average is ", average)
