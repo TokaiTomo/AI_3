b7aabc82ede12cf20a1069d4d3ed8b4f1d83df6e
TokaiTomo <tokai.tomonari@gmail.com>
2025-09-15 18:57:48 +0700

    commit!!!!!

--- birthday.py
+++ birthday.py
@@ -1,26 +1,26 @@
 def b_1(list_1):
     list_1.append(input('Input birthday: '))
-    print(list_1)
+ 
 def b_2(list_1):
     list_1.append(input('Input birthday: '))
     print(list_1)
 def b_3(list_1):
     list_1.append(input('Input birthday: '))
-    print(list_1)
+  
 def b_3(list_1):
     list_1.append(input('Input birthday: '))
-    print(list_1)
+  
 
 def b_4(list_1):
     list_1.append(input('Input birthday: '))
-    print(list_1)
+   
 def b_5(list_1):
-
     list_1.append(input('Input birthday: '))
-    print(list_1)
-list_1 = ()
+
+list_1 = []
 b_1(list_1)
 b_2(list_1)
 b_3(list_1)
 b_4(list_1)
 b_5(list_1)
+print(list_1)
