88b2d5b424209fc1f327bbcf9b293c36c3965f47
TokaiTomo <tokai.tomonari@gmail.com>
2025-10-29 19:09:29 +0700

    pattern

--- right_angle_triangle.py
+++ right_angle_triangle.py
@@ -1,6 +1,6 @@
 print("half pyramid patterns of stars")
-n = int(input("Number of the rows you want: "))
-for i in range(n):
+rows = int(input("Number of the rows you want: "))
+for i in range(rows):
     for j in range(i+1):
         print("*", end=" ")
     print()
