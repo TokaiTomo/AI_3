e9c87c3c0051addc2d338eb2ce86ef2a895fbcfb
TokaiTomo <tokai.tomonari@gmail.com>
2025-11-09 19:01:08 +0700

    function

---
+++ add_sub.py
@@ -0,0 +1,16 @@
+def add(p,q):
+    ans=p+q
+    return ans
+def sub(p,q):
+    ans = p-q
+    return ans
+def mult(p,q):
+    ans=p*q
+    return ans
+def divide(p,q):
+    ans = p/q
+    return ans
+add(1,2)
+sub(10,3)
+mult(2,4)
+divide(55,11)
