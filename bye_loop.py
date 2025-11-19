a3ee05681372f1dc6aa4e06afd522a626d66ca13
TokaiTomo <tokai.tomonari@gmail.com>
2025-11-19 19:06:41 +0700

    loop

---
+++ bye_loop.py
@@ -0,0 +1,20 @@
+valid = False
+while not valid:
+    try:
+        num= int(input("Enter a number: "))
+        while num%2 == 0:
+            #print("bye")
+            valid = True
+    except ValueError:
+        print("Error")
+
+valid = False #loop runs
+while not valid: # same as While true
+    try:
+        n = int(input("Enter a number: "))
+#enter a even number
+        while n%2 == 0:
+            #print("bye")
+            valid = True # loop stops
+    except ValueError:
+        print("Invalid")
